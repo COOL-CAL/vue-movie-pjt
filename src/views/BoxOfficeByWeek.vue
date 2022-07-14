06eb7892d5e3ed5f01695512a492d883c8ce7c0c
COOL_CAL <grin0330@gmail.com>
2022-07-14 18:03:31 +0900

    누적관객수, 누적매출액 콤마 찍기

--- src/views/BoxOfficeByWeek.vue
+++ src/views/BoxOfficeByWeek.vue
@@ -5,36 +5,22 @@
       <input type="date" v-model="selectedDate">
       <button @click="search">Search</button>
     </div>
-    <table>
-      <thead>
-        <tr>
-          <th>순위</th>
-          <th>제목</th>
-          <th>개봉일</th>
-          <th>누적관객수</th>
-          <th>누적매출액</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in list" :key="item.movieCd">
-          <td>{{ item.rank }}</td>
-          <td>{{ item.movieNm }}</td>
-          <td>{{ item.openDt }}</td>
-          <td>{{ item.audiAcc }}</td>
-          <td>{{ item.salesAcc }}</td>
-        </tr>
-      </tbody>
-    </table>
+    <RankTable :list="list"></RankTable>
   </div>
 </template>
 
 <script>
+import RankTable from '../components/boxoffice/RankTable';
+
 export default {
   data() {
     return {
       selectedDate: '',
       list: []
     }
+  },
+  components: {
+    RankTable
   },
   methods: {
     search() {
